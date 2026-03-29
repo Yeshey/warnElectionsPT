2882bbfb2eeabc0628269bd12d7028c7210907ea
Yeshey <yesheysangpo@gmail.com>
2026-03-29 18:34:30 +0100

    base react native project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # warnElections
 App to warn with notifications about upcoming elections for Portuguese citizens
+
+To run your project, navigate to the directory and run one of the following npm commands.
+
+- npm run android
+- npm run ios # you need to use macOS to build the iOS project - use the Expo app if you need to do iOS development without a Mac
+- npm run web
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { systems, nixpkgs, ... } @ inputs:
+  let
+    eachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (
+        system:
+          f nixpkgs.legacyPackages.${system}
+      );
+  in {
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_25
+          android-tools
+          jdk17
+        ];
+
+        shellHook = ''
+          echo "Env ready"
+        '';
+
+        #ANDROID_HOME = "${pkgs.android-tools}/share/android-tools";
+        #JAVA_HOME = "${pkgs.jdk17}";
+      };
+    });
+  };
+}
